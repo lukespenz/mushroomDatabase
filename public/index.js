0b1276c6be3b4a84ddf6db9a138ba3158707298e
Lucas Spencer <lucas.spencer14@gmail.com>
2022-05-03 20:26:57 -0600

    index css dont work

--- public/index.js
+++ public/index.js
@@ -1,13 +1,12 @@
 console.log('runn');
 
-// deepai.setApiKey('c8520fde-6457-46c3-aadd-dcd68e931235');
+const addMushroom = () => {
+    console.log('clicked')
+    window.location.replace(addPage)
+}
 
-// (async function() {
-//     var resp = await deepai.callStandardApi("deepdream", {
-//             image: "https://imgs.search.brave.com/V-JVLTtOaQdvOJe2DC_2SHF-QS9derdQkD4-xKLoSn8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9oZGRl/c2t0b3B3YWxscGFw/ZXJzLmluL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDE1LzA5L2Nh/dC1leWVzLWN1dGUu/anBn"
-//     });
-//     await deepai.renderResultIntoElement(result, document.getElementById('yourResultContainerId'));
+// button.addEventListener('addMushroom', addMushroom)
 
-//     console.log(resp.data);
-//     console.log('deepai working?')
-// });
+const loginPage = `/public/index.html`
+const addPage = `/public/addMushroom.html`
+const viewPage =`/public/viewMushrooms.html`
--- server/index.js
+++ server/index.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require("express");
+// const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+// app.use(cors());
 
 app.use(express.static('../public'))
-app.use('/styles.css', express.static(__dirname + '../public/styles.css'))
+app.use('/index.css', express.static(__dirname + '../public/index.css'))
+app.use('/view.css', express.static(__dirname + '../public/view.css'))
 app.use('/index.js', express.static(__dirname + '../public/index.js'))
 app.use('/deepai.min.js', express.static(__dirname + '../public/deepai.min.js'))
 
 app.get('', (req, res) => {res.sendFile(__dirname + '../public/index.html')})
 
-const port = process.env.PORT || 3005
+const port = 5500 //|| process.env.PORT
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
